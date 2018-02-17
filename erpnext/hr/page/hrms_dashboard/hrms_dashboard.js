c80178d1fd395892f5a34ae288911866a2da9a9b
Vignesh <vigneshwaran@valiantsystems.com>
2018-02-17 11:08:11 +0530

    FIRST

---
+++ erpnext/hr/page/hrms_dashboard/hrms_dashboard.js
@@ -0,0 +1,11 @@
+frappe.pages['hrms-dashboard'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Dashboard',
+		single_column: true
+	});
+
+	
+	page.main.html(frappe.render_template("ht"));
+
+}
